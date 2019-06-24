f9b533de1638ee7692d147917c81938bd6435aaf
Matheus de Andrade Silva <matheus.mas132@hotmail.com>
2019-06-24 15:22:00 -0300

    adding backtracking code

--- src/player.cpp
+++ src/player.cpp
@@ -1,12 +1,82 @@
 #include "player.h"
 
-bool Player::find_way( void )
+bool Player::find_solution( std::pair< int, int > food_loc, std::pair< int, int > player_loc, char** matrix )
 {	
 	// cleaning deque
 	this->foodWay.clear();
 
 	// backtracking
-	std::deque< std::queue< std::pair< int, int > > > bt_layers;
+	std::deque< std::pair< int, int > > this->foodWay;
+	std::set< std::pair< int, int > > visited;
+
+	this->foodWay.push_back( std::make_pair(player_loc.first, player_loc.second) );
+	std::pair< int, int > current (player_loc.first, player_loc.second);
+	visited.insert( current );
+
+	while( this->foodWay.back() != food_loc and this->foodWay.empty() == false )
+	{
+		std::pair< int, int > aux;
+		bool findOutlet = false;
+		{
+			if( matrix[current.first + 1][current.second] == ' ' and visited.count( std::make_pair(current.first + 1, current.second) ) == 0)
+			{
+				findOutlet = true;
+				aux = std::make_pair(current.first + 1, current.second);
+			}
+			else if( matrix[current.first - 1][current.second] == ' ' and visited.count( std::make_pair(current.first - 1, current.second) ) == 0)
+			{
+				findOutlet = true;
+				aux = std::make_pair(current.first - 1, current.second);
+			}
+			else if( matrix[current.first][current.second + 1] == ' ' and visited.count( std::make_pair(current.first, current.second + 1) ) == 0)
+			{
+				findOutlet = true;
+				aux = std::make_pair(current.first, current.second + 1);
+			}
+			else if( matrix[current.first][current.second - 1] == ' ' and visited.count( std::make_pair(current.first, current.second - 1) ) == 0)
+			{
+				findOutlet = true;
+				aux = std::make_pair(current.first, current.second - 1);
+			}
+			// ----------------------------------
+			if( matrix[current.first + 1][current.second] == 'f')
+			{
+				aux = std::make_pair(current.first + 1, current.second);
+				this->foodWay.push_back(aux);
+				break;
+			}
+			else if( matrix[current.first - 1][current.second] == 'f')
+			{
+				aux = std::make_pair(current.first - 1, current.second);
+				this->foodWay.push_back(aux);
+				break;
+			}
+			else if( matrix[current.first][current.second + 1] == 'f')
+			{
+				aux = std::make_pair(current.first, current.second + 1);
+				this->foodWay.push_back(aux);
+				break;
+			}
+			else if( matrix[current.first][current.second - 1] == 'f')
+			{
+				aux = std::make_pair(current.first, current.second - 1);
+				this->foodWay.push_back(aux);
+				break;
+			}
+		}
+		if(findOutlet)
+		{
+			this->foodWay.push_back(aux);
+			visited.insert(aux);
+		}
+		else
+		{
+			this->foodWay.pop_back();
+		}
+		if( this->foodWay.empty() )
+			return false;
+		current = this->foodWay.back();
+	}
 
 	return true;
 }
