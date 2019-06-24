28fa3f869df710aaaac0a0b78d7b654fea3c4209
felipecolares22 <felipe.colares@hotmail.com>
2019-06-24 14:24:41 -0300

    Snakegame::game_over() minor tweaks

--- src/snakegame.cpp
+++ src/snakegame.cpp
@@ -5,11 +5,11 @@ void Snakegame::initialize_game( int argc, char* argv[] )
 	level.get_input( argc, argv );
 	printmenu(level.num_levels(), 5, 0, 10);
 	level.print_level();
-	game_over(30000);
+	game_over();
 }
 
 void Snakegame::game_over( )
 {
-	printendgame( score );
+	printendgame( score );	
 }
 
